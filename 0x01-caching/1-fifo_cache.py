424e2d9041da9c720fc46eef150ca5ef7e668018
Ernyson <ernestaiji1@gmail.com>
2023-04-19 15:46:29 +0000

    caching - FIFO

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,35 +1,39 @@
-#!/usr/bin/env python3
-"""FIFO Caching
-"""
-from collections import OrderedDict
+#!/usr/bin/python3
+"""FIFO Caching"""
 
 
-BaseCaching = __import__('base_caching').BaseCaching
-
+from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
     """class that inherits from
     BaseCaching
     """
-    def __init__(self):
-        """Initialize
-        """
 
+    def __init__(self):
+        """initializes the cache
+        """
         super().__init__()
-        self.cache_data = OrderedDict()
+        self.load = []
 
     def put(self, key, item):
         """item in cache
         """
-        if key is None or item is None:
-            return
-        self.cache_data[key] = item
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first_key, _ = self.cache_data.popitem(False)
-            print("DISCARD:", first_key)
+        if key and item:
+            if key in self.cache_data.keys():
+                self.cache_data[key] = item
+            elif len(self.cache_data.keys()) < self.MAX_ITEMS:
+                self.cache_data[key] = item
+                self.load.append(key)
+            else:
+                self.cache_data[key] = item
+                discard = self.load[0]
+                del self.cache_data[self.load[0]], self.load[0]
+                self.load.append(key)
+                print(f'DISCARD: {discard}')
 
     def get(self, key):
         """now get a key from the cache
         """
-        return self.cache_data.get(key, None)
+        if key:
+            return self.cache_data.get(key, None)
